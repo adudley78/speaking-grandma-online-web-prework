4a891b2808db47a8ffa0d827eb747ed2ee7bca58
Adam Dudley <dudleyadam@gmail.com>
2018-11-21 20:19:40 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,15 +1,15 @@
-def speak_to_grandma(what_I_say)
+def speak_to_grandma(what_I_say, my_name)
   if what_I_say == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
   if what_I_say == what_I_say.upcase
     return "NO, NOT SINCE 1938!"
   elsif what_I_say != what_I_say.upcase
-    return "HUH?! SPEAK UP, SONNY!"
+    return "HUH?! SPEAK UP" + my_name + "!"
   end
 end
 
 
-phrase = speak_to_grandma("what")
+phrase = speak_to_grandma("what", "Bob")
 
 puts phrase
