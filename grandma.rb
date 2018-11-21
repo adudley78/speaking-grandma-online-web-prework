13d46e2a71b2e20494a38cba32b32f44ad500c17
Adam Dudley <dudleyadam@gmail.com>
2018-11-21 20:19:55 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(what_I_say, my_name)
     return "I LOVE YOU TOO PUMPKIN!"
   end
   if what_I_say == what_I_say.upcase
-    return "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!" 
   elsif what_I_say != what_I_say.upcase
     return "HUH?! SPEAK UP" + my_name + "!"
   end
